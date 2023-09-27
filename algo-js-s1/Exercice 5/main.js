045178378adf5f688efb079ff823243d102cbe1b
AnthoAY <146062266+AnthoAY@users.noreply.github.com>
2023-09-27 09:12:56 +0200

    Add files via upload
    new exercice

---
+++ algo-js-s1/Exercice 5/main.js
@@ -0,0 +1,15 @@
+var a = "Jean"
+var b = "Paul"
+var result = CheckName (a, b)
+
+function CheckName (userName1, userName2){
+    if (userName1 === userName2){
+        return (true)
+    }
+
+    else 
+        return (false)
+
+}
+
+console.log(result)
