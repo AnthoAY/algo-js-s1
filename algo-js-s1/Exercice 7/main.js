0dc6eaa1c83e9d9d92f1734b14fe2581a932e5e7
AnthoAY <146062266+AnthoAY@users.noreply.github.com>
2023-09-27 11:24:52 +0200

    Add files via upload
    new exercice

---
+++ algo-js-s1/Exercice 7/main.js
@@ -0,0 +1,17 @@
+/* crée un objet d'un personnage choisi, 
+dans l'objet, nom prénom age. + afficher
+dans la console une phrase ou il y a écrit 
+"Gabriel Monier à 28 ans et est a l'école"
+*/
+
+
+let Bangalore = {
+    firstName: "anita",
+    lastName: " williams",
+    classe: "assault",
+    Age: 40
+}
+
+let sentence = " Bangalore de classe " + Bangalore.classe + " se nomme " + Bangalore.firstName + Bangalore.lastName + " et est agée de " + Bangalore.Age + " ans."
+
+console.log(sentence)
